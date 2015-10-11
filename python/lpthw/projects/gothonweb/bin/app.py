97c1c1d287c27944c2012b9823cf3efe174b4dff
NickHuang <xjhznick@gmail.com>
2015-10-11 22:17:50 +0800

    A simple web project

---
+++ python/lpthw/projects/gothonweb/bin/app.py
@@ -0,0 +1,15 @@
+import web
+
+urls = ('/', 'Index')
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class Index(object):
+    def GET(self):
+        greeting = "Hello, Nick"
+        return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+    app.run()
